7ba90c8029640b91dd4e31e671659d8cf7bf4df2
IchchadhariNaag <ralphgbartley@gmail.com>
2021-03-04 15:55:06 +0000

    wrapping collections upping capacity to 20
    Co-authored-by: Liz <lizzy_boucher@hotmail.com>
    Co-authored-by: Ralph <ralphgbartley@gmail.com>

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,18 @@
 require_relative 'Bike'
 class DockingStation
-attr_accessor :bike
+attr_accessor :bikes
 
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.length == 0
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Station is full' if @bike
-    @bike = bike
+    fail 'Station is full' if @bikes.length >= 20
+    @bikes << bike
   end
 end
